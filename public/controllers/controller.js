1321ffabf9c57caae30a4462b1b2b96e134c89f0
i313404 <shamil.fayzullin@sap.com>
2017-04-27 10:34:39 +0200

    add refresh functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,13 +1,20 @@
 function AppCtrl($scope, $http) {
 	console.log("Hello world from controller");
+	var refresh = function() {
 	$http.get('/bookings').success(function(response) {
 		console.log("I got the data I requested");
 		$scope.bookings = response;
+		$scope.booking = "";
 	});
+	};
+	
+	refresh();
+
 	$scope.addBooking = function() {
 		console.log($scope.booking);
 		$http.post('/bookings', $scope.booking).success(function(response) {
     	console.log(response);
+    	refresh();
     	});
   };
 }
